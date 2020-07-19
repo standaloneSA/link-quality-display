0d7edc6efc53ae77fcccddb158441f779a2000de
Matt Simmons <standalone.sysadmin@gmail.com>
2020-07-19 13:33:02 -0700

    Added second line

--- graphics.py
+++ graphics.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python3
-from samplebase import SampleBase
+from config import Config
 from rgbmatrix import graphics
+import datetime
 import time
 
 
-class GraphicsTest(SampleBase):
+class GraphicsTest(Config):
     def __init__(self, *args, **kwargs):
         super(GraphicsTest, self).__init__(*args, **kwargs)
 
     def run(self):
         canvas = self.matrix
         font = graphics.Font()
-        font.LoadFont("../../../fonts/7x13.bdf")
+        font.LoadFont("fonts/tom-thumb.bdf")
 
-        red = graphics.Color(255, 0, 0)
-        graphics.DrawLine(canvas, 5, 5, 22, 13, red)
+        msgFont = graphics.Font()
+        msgFont.LoadFont("fonts/clR6x12.bdf")
 
-        green = graphics.Color(0, 255, 0)
-        graphics.DrawCircle(canvas, 15, 15, 10, green)
+        #red = graphics.Color(255, 0, 0)
+        #graphics.DrawLine(canvas, 5, 5, 22, 13, red)
+
+        #green = graphics.Color(0, 255, 0)
+        #graphics.DrawCircle(canvas, 15, 15, 10, green)
 
         blue = graphics.Color(0, 0, 255)
-        graphics.DrawText(canvas, font, 2, 10, blue, "Text")
+        red = graphics.Color(255,0,0)
 
-        time.sleep(10)   # show display for 10 seconds before exit
+        while True:
+            graphics.DrawText(canvas, font, 4, 5, blue, str(datetime.datetime.now().strftime("%H:%M:%S")))
+            graphics.DrawText(canvas, msgFont, 4, 20, red, "Line 2")
+            time.sleep(0.5) 
+            canvas.Clear()
 
 
 # Main function
